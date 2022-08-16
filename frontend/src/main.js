3e62fdf2a9fe2d5f34f297ceead6740db9ea1d20
Daniel Choi <schoiak@connect.ust.hk>
2022-08-16 17:51:30 +0900

    Remove - temporary delete

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-
-import './assets/css/tailwind.css'
-// import './assets/css/tailwind.output.css'
-
-createApp(App).use(store).mount('#app')
