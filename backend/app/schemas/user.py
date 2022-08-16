32bb0f7132ddbf50404633655ddaa4b7f2cfa655
Daniel Choi <schoiak@connect.ust.hk>
2022-08-16 23:25:55 +0900

    Edit - user schema changed

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -3,19 +3,40 @@ from typing import Dict, List, Optional
 from pydantic import BaseModel, EmailStr, Field
 
 
-class UserEmail(BaseModel):
+class UserBase(BaseModel):
     email: EmailStr
-
-
-class UserBase(UserEmail):
     first_name: Optional[str]
     last_name: Optional[str]
     birthday: Optional[datetime]
     is_superuser: bool = False
 
 
-class UserPost(UserBase):
-    passwordHash: str
+class UserCreate(UserBase):
+    password: str
+
+
+class UserInDBBase(UserBase):
+    class Config:
+        orm_mode = True
+
+
+class UserReturn(UserBase):
+    email: EmailStr
+    first_name: Optional[str]
+    last_name: Optional[str]
+    birthday: Optional[datetime]
+    is_superuser: bool = False
+
+# Additional properties stored in DB but not returned by API
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    ...
 
 
 class UserResponseBase(BaseModel):
