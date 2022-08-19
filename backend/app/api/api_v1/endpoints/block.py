a4de0b57da8f84b3742719ba877686355dd95638
Daniel Choi <schoiak@connect.ust.hk>
2022-08-19 22:23:42 +0900

    Add - creating block api added

--- backend/app/api/api_v1/endpoints/block.py
+++ backend/app/api/api_v1/endpoints/block.py
@@ -1,21 +1,42 @@
-from fastapi import APIRouter
-from app.schemas.block import BlockCreate
-from app.crud.crud_block import CRUDBlock
+from datetime import datetime
+from fastapi import APIRouter, Depends, status
+from app import schemas
+from app.schemas.block import BlockCreate, BlockInDB
+from app import crud
+from app.api import deps
+from pymongo.client_session import ClientSession
+
 router = APIRouter()
 
 
-@router.get("/{blockId}", status_code=202)
+@router.get("/{blockId}", status_code=status.HTTP_201_CREATED)
 def getBlock(blockId: str, start: int = 0, end: int = 0) -> dict:
     # validateStorage()
     # validateBlock()
-    return CRUDBlock.read_many(blockId, start, end)
+    return crud.block.read_many(blockId, start, end)
 
 
-@router.post("/", status_code=202)
-def makeBlock(block: BlockCreate) -> dict:
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_block(
+    storage_id: str,
+    block_in: BlockCreate,
+    session: ClientSession = Depends(deps.get_session)
+) -> schemas.InsertResponse:
     """
     Post Block
     """
     # validateStorage()
     # validateBlock()
-    return CRUDBlock.create(block)
+
+    # fileType: str
+    # fileUrl: str
+    # size
+    data = BlockInDB(
+        **block_in.dict(),
+        creation_date=datetime.utcnow(),
+        file_type='ss',
+        file_url="ss",
+        size=5
+    )
+    insertResult = crud.block.create(storage_id, data, session)
+    return insertResult
