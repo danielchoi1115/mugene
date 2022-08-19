2cb36139484de44a2e5739d7689254504d634c65
Daniel Choi <schoiak@connect.ust.hk>
2022-08-19 22:26:06 +0900

    Mod - removed unused class

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -19,14 +19,6 @@ class UserInDBBase(UserBase):
     class Config:
         orm_mode = True
 
-
-class UserReturn(UserBase):
-    email: EmailStr
-    first_name: Optional[str]
-    last_name: Optional[str]
-    birthday: Optional[datetime]
-    is_superuser: bool = False
-
 # Additional properties stored in DB but not returned by API
 
 
@@ -37,16 +29,3 @@ class UserInDB(UserInDBBase):
 # Additional properties to return via API
 class User(UserInDBBase):
     ...
-
-
-class UserResponseBase(BaseModel):
-    result: bool
-
-
-class UserResponse(UserResponseBase):
-    user_id: str = Field(alias='_id')
-
-
-class UserResponseError(UserResponseBase):
-    result: bool = False
-    exception: str
