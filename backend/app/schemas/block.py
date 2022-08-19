cdc689754af9e48a38f604dc99c951e8336d7f5b
Daniel Choi <schoiak@connect.ust.hk>
2022-08-19 22:27:34 +0900

    Mod - seperated user requirements

--- backend/app/schemas/block.py
+++ backend/app/schemas/block.py
@@ -1,29 +1,33 @@
 from datetime import datetime
 from typing import List
 from pydantic import BaseModel, Field
-from app.schemas.dbref import DBRefBase
+from app.schemas.dbref import DBRefBase, RefBlock
+from .pyobjectid import PyObjectId
+from .member import Member
 # from bson import DBRef
 
 
 class BlockBase(BaseModel):
     name: str
-    size: int = Field(gt=0, description="The file size must be greater than zero")
-    fileUrl: str
-
-    class Config:
-        arbitrary_types_allowed = True
+    is_folder: bool
 
 
 class BlockCreate(BlockBase):
-    name: str
-    isFolder: bool
-    creationDate: datetime
-    fileType: str
-    size: int = Field(gt=0, description="The file size must be greater than zero")
-    fileUrl: str
-    parentFolder: DBRefBase | None = None
-    childFolders: List[DBRefBase] = []
-    created_by: str
+    created_by: Member
+    parent_folder: RefBlock = None
 
     class Config:
         arbitrary_types_allowed = True
+
+
+class BlockInDB(BlockCreate):
+    creation_date: datetime
+    file_type: str
+    file_url: str
+    size: int = Field(gt=0, description="The file size must be greater than zero")
+    child_folders: List[RefBlock] = []
+
+
+class BlockParent(BaseModel):
+    parent_folder: PyObjectId = Field(default_factory=PyObjectId, alias="parent_folder.$id")
+
