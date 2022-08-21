8310a1a35277a83ba11f56f46a639b61803d448b
Daniel Choi <schoiak@connect.ust.hk>
2022-08-21 23:24:19 +0900

    Add - reading multiple blocks added

--- backend/app/crud/crud_block.py
+++ backend/app/crud/crud_block.py
@@ -1,6 +1,7 @@
+from typing import List
 from app import exceptions
 from app.db.init_client import client
-from app.schemas.block import BlockCreate, BlockParent
+from app.schemas.block import Block, BlockInDB, BlockParent
 from bson import json_util
 import json
 # from bson.dbref import DBRef
@@ -10,13 +11,14 @@ import json
 from bson import ObjectId
 
 from app import schemas
+from app.schemas.pyobjectid import PyObjectId
 
 
 class CRUDBlock():
-    def create(
+    async def create(
         self,
         storage_id: str,
-        block_in_db: BlockCreate,
+        block_in_db: BlockInDB,
         session: ClientSession
     ) -> schemas.InsertResponse:
         insert_result = False
@@ -25,6 +27,7 @@ class CRUDBlock():
             res = collection.find_one(block_in_db.parent_folder.refid)
             if not res:
                 raise exceptions.NoParentFolderException
+
         try:
             session.start_transaction()
             res = collection.insert_one(
@@ -44,18 +47,22 @@ class CRUDBlock():
                 exception=str(ex)
             )
 
-    def read_many(blockId: str, start: int = 0, end: int = 0, projection: dict = None):
-        res = client['storage_db']['62f3d5e306d48ff2df0dc4fa'].find_one(
-            {'_id': ObjectId(blockId)}
+    def read_many(self, workspace_id: str, parent_id: PyObjectId = None, start: int = 0, end: int = 0) -> Block:
+        res = client['storage_db'][workspace_id].find(
+            {"parent_folder.$id": parent_id}
         )
         if end - start > 10:
             end = start + 9
         if end < start:
             start = end
-        res2 = client['storage_db']['62f3d5e306d48ff2df0dc4fa'].find(
-            {}, limit=end-start+1, skip=start, projection={"_id": 1, "name": 1}
-        )
-        return res
+        # res2 = client['storage_db']['62f3d5e306d48ff2df0dc4fa'].find(
+        #     {}, limit=end-start+1, skip=start, projection={"_id": 1, "name": 1}
+        # )
+        read_result: List[Block] = []
+        for i in res:
+            read_result.append(Block(**i))
+
+        return [block.dict() for block in read_result]
 
 
 block = CRUDBlock()
