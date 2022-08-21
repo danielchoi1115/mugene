2ece2bc65f43b350915a830294728650c873b105
Daniel Choi <schoiak@connect.ust.hk>
2022-08-21 23:23:30 +0900

    Add - reading multiple blocks added

--- backend/app/api/api_v1/endpoints/block.py
+++ backend/app/api/api_v1/endpoints/block.py
@@ -1,26 +1,44 @@
 from datetime import datetime
-from fastapi import APIRouter, Depends, status
+from typing import List, Optional
+from fastapi import APIRouter, Depends, status, UploadFile, File, Form
 from app import schemas
-from app.schemas.block import BlockCreate, BlockInDB
+from app.schemas.block import BlockIn, BlockInDB
 from app import crud
 from app.api import deps
 from pymongo.client_session import ClientSession
-
+import os
+from app.schemas.dbref import RefBlock, RefUser
+from app import exceptions
+from app.schemas.member import Member
+from app.schemas.pyobjectid import PyObjectId
+from app.schemas.workspace import Workspace
 router = APIRouter()
 
 
-@router.get("/{blockId}", status_code=status.HTTP_201_CREATED)
-def getBlock(blockId: str, start: int = 0, end: int = 0) -> dict:
+@router.get("/", status_code=status.HTTP_200_OK)
+def getBlock(
+    parent_id: PyObjectId = None,
+    start: int = 0,
+    end: int = 10,
+    workspace_id: str = Depends(deps.get_current_workspace)
+) -> dict:
     # validateStorage()
     # validateBlock()
-    return crud.block.read_many(blockId, start, end)
+    read_result = crud.block.read_many(
+        workspace_id=workspace_id,
+        parent_id=parent_id,
+        start=start,
+        end=end
+    )
+    return str(read_result)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_block(
+async def create_block(
     storage_id: str,
-    block_in: BlockCreate,
-    session: ClientSession = Depends(deps.get_session)
+    block_in: BlockIn = Depends(BlockIn),
+    session: ClientSession = Depends(deps.get_session),
+    file: UploadFile = File(None)
 ) -> schemas.InsertResponse:
     """
     Post Block
@@ -28,15 +46,37 @@ def create_block(
     # validateStorage()
     # validateBlock()
 
-    # fileType: str
-    # fileUrl: str
-    # size
+    file_type = 'directory'
+    file_url = None
+    size = None
+
+    user_ref = RefUser(refid=block_in.creator_id)
+    created_by = Member(user_ref=user_ref)
+
+    if file and block_in.is_folder:
+        raise exceptions.FileAsFolderException
+    elif file is None and not block_in.is_folder:
+        raise exceptions.FileIsNullException
+
+    if file:
+        content = await file.read()
+        size = len(content)
+        filename, file_type = os.path.splitext(file.filename)
+        file_url = filename
+
+    parent_folder = None
+    if block_in.parent_folder_id:
+        parent_folder = RefBlock(refid=block_in.parent_folder_id)
+
     data = BlockInDB(
-        **block_in.dict(),
+        name=block_in.name,
+        is_folder=block_in.is_folder,
+        created_by=created_by,
+        parent_folder=parent_folder,
         creation_date=datetime.utcnow(),
-        file_type='ss',
-        file_url="ss",
-        size=5
+        file_type=file_type,
+        file_url=file_url,
+        size=size
     )
-    insertResult = crud.block.create(storage_id, data, session)
+    insertResult = await crud.block.create(storage_id, data, session)
     return insertResult
--- backend/app/schemas/block.py
+++ backend/app/schemas/block.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 from app.schemas.dbref import DBRefBase, RefBlock
 from .pyobjectid import PyObjectId
 from .member import Member
+from fastapi import Form
 # from bson import DBRef
 
 
@@ -12,22 +14,47 @@ class BlockBase(BaseModel):
     is_folder: bool
 
 
-class BlockCreate(BlockBase):
-    created_by: Member
-    parent_folder: RefBlock = None
+class BlockIn(BlockBase):
+    name: str = Form()
+    is_folder: bool = Form()
+    creator_id: PyObjectId = Form()
+    parent_folder_id: PyObjectId = Form(None)
 
-    class Config:
-        arbitrary_types_allowed = True
+    # class Config:
+    #     arbitrary_types_allowed = True
+    # @classmethod
+    # def as_form(
+    #     cls,
+    #     name: str = Form(),
+    #     is_folder: bool = Form(),
+    #     creator_id: PyObjectId = Form(),
+    #     parent_folder_id: PyObjectId = Form(None),
+
+    # ) -> BlockIn:
+    #     return cls(
+    #         name=name,
+    #         is_folder=is_folder,
+    #         creator_id=creator_id,
+    #         parent_folder_id=parent_folder_id
+    #     )
 
 
-class BlockInDB(BlockCreate):
+class BlockInDB(BlockBase):
     creation_date: datetime
-    file_type: str
-    file_url: str
-    size: int = Field(gt=0, description="The file size must be greater than zero")
-    child_folders: List[RefBlock] = []
+    created_by: Member
+    file_type: Optional[str]
+    file_url: Optional[str]
+    size: Optional[int] = Field(default=None, gt=0, description="The file size must be greater than zero")
+    parent_folder: Optional[DBRefBase] = None
+    child_folders: List[DBRefBase] = []
+    
+
+
+class Block(BlockInDB):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+
 
 
 class BlockParent(BaseModel):
     parent_folder: PyObjectId = Field(default_factory=PyObjectId, alias="parent_folder.$id")
-
