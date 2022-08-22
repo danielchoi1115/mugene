931c1b8e36f4a1e8f937d26527954b77291c1a47
Daniel Choi <schoiak@connect.ust.hk>
2022-08-22 23:12:18 +0900

    Test - middleware added for testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, APIRouter
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi import FastAPI, APIRouter, Request
+import time
 from app.api.api_v1 import api
 from app.core.config import settings
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.core.middleware import BaseMiddleware
 app = FastAPI(
     title="Recipe API", openapi_url="/openapi.json"
 )
@@ -20,8 +22,28 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+# @app.middleware("http")
+# async def add_process_time_header(request: Request, call_next):
+#         try:
+#             start_time = time.time()
+#             response = await call_next(request)
 
-# 5
+#         # except InvalidId as ex:
+#         #     response = JSONResponse(
+#         #         content=jsonable_encoder({
+#         #             "result": "failed",
+#         #             "error": str(ex)
+#         #         }),
+#         #         status_code=400
+#         #     )
+#         except Exception as ex:
+#             print(ex)
+#         finally:
+#             process_time = time.time() - start_time
+#             response.headers["X-Process-Time"] = str(process_time)
+#             return response
+
+
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
