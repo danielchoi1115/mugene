418eb34e43ec65db9427c761da7788e1f52bc053
Daniel Choi <schoiak@connect.ust.hk>
2022-08-26 23:40:18 +0900

    Mod - member api added

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import user, block, workspace, auth
+from app.api.api_v1.endpoints import user, block, workspace, auth, member
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(block.router, prefix="/block", tags=["block"])
 api_router.include_router(workspace.router, prefix="/workspace", tags=["workspace"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(member.router, prefix="/member", tags=["member"])
