8b028d501c6ea6c80dae98ceb84175f436ce68b6
Daniel Choi <schoiak@connect.ust.hk>
2022-08-27 22:39:39 +0900

    Mod - edited type checking for type safty

--- backend/app/crud/crud_member.py
+++ backend/app/crud/crud_member.py
@@ -5,13 +5,17 @@ from app.crud.base import CRUDBase
 from app import models
 from app.schemas.member import MemberCreate, MemberUpdate
 from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
 
 class CRUDMember(CRUDBase[models.Member, MemberCreate, MemberUpdate]):
     def get(self, db: Session, id: Any) -> Optional[models.Member]:
         return db.query(self.model).filter(self.model.member_id == id).first()
     
     def create(self, db: Session, *, obj_in: MemberCreate) -> models.Member:
-        create_data = obj_in.dict()
+        if isinstance(obj_in, dict):
+            create_data = obj_in
+        else:
+            create_data = obj_in.dict(exclude_unset=True)
         db_obj = models.Member(**create_data)
         db_obj.join_date = datetime.utcnow()
         db.add(db_obj)
