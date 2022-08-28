cdbb21f9d8a9da07b379ac053a226a324b6d6c68
Daniel Choi <schoiak@connect.ust.hk>
2022-08-28 23:56:56 +0900

    Mod - added update method

--- backend/app/crud/crud_workspace.py
+++ backend/app/crud/crud_workspace.py
@@ -1,35 +1,33 @@
 
 from datetime import datetime
-from typing import List
+from typing import Any, Dict, List
 from app import schemas
+from app.crud.base import CRUDBase
 from app.db.init_client import client
 from pymongo.client_session import ClientSession
 from app import models
 from sqlalchemy.orm import Session
 
-class CRUDWorkspace():
-    def create(self, db: Session, *, user_in: models.User, obj_in: schemas.WorkspaceCreate) -> models.User:
+class CRUDWorkspace(CRUDBase[models.Workspace, schemas.WorkspaceCreate, schemas.WorkspaceUpdate]):
+    def create(self, db: Session, *, user_in: models.User, obj_in: schemas.WorkspaceCreate) -> models.Workspace:
         create_data = obj_in.dict()
         db_obj = models.Workspace(**create_data)
-        db_obj.created_by = user_in.user_id
-        db_obj.creation_date = datetime.now()
+        db_obj.creator_id = user_in.user_id
+        db_obj.owner_id = user_in.user_id
+        
+        db_obj.date_created = datetime.utcnow()
         db.add(db_obj)
         db.commit()
         return db_obj 
   
-    def update(self, filter, update, session: ClientSession, upsert=True) -> schemas.UpdateResponse:
-        try:
-            session.start_transaction()
-            res = client['workspace_db']['workspaces'].update_many(
-                filter,
-                update,
-                upsert=False
-            )
+    def update(
+        self, db: Session, *, db_obj: models.Workspace, obj_in: schemas.WorkspaceUpdate| Dict[str, Any]
+    ) -> models.Workspace:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
 
-            session.commit_transaction()
-            insert_result = res
-            return None
-        except Exception as ex:
-            session.abort_transaction()
-            raise
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
+    
 workspace = CRUDWorkspace()
