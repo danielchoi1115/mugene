35bc25476e624be1b9acb3891a8dca791c161a66
Daniel Choi <schoiak@connect.ust.hk>
2022-09-01 00:05:37 +0900

    Mod - report endpoint added

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import user, block, workspace, auth, member
+from app.api.api_v1.endpoints import user, block, workspace, auth, member, report
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user", tags=["user"])
@@ -8,3 +8,4 @@ api_router.include_router(block.router, prefix="/block", tags=["block"])
 api_router.include_router(workspace.router, prefix="/workspace", tags=["workspace"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(member.router, prefix="/member", tags=["member"])
+api_router.include_router(member.router, prefix="/report", tags=["report"])
