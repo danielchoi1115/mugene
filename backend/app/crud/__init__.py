2f94a9745858766632669010405978d0fff1bde1
Daniel Choi <schoiak@connect.ust.hk>
2022-09-02 00:49:14 +0900

    Mod - init class edited

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -2,3 +2,5 @@ from .crud_user import user
 from .crud_workspace import workspace
 from .crud_block import block
 from .crud_member import member
+from .crud_report import report
+from .crud_reportdata import reportdata
--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -3,3 +3,5 @@ from .response import InsertResponse, InsertResponseError, UpdateResponse
 from .member import MemberCreate, MemberOut, MemberUpdate
 from .workspace import WorkspaceOut,WorkspaceCreate, WorkspaceUpdate
 from .block import BlockCreate, BlockOut
+from .report import ReportCreate, ReportUpdate, ReportOut
+from .reportdata import ReportdataCreate, ReportdataUpdate
