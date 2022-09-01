090fb3b54333141f619ac299885f4540dc1f0a2c
Daniel Choi <schoiak@connect.ust.hk>
2022-09-02 00:45:23 +0900

    Mod - report create API updated

--- backend/app/api/api_v1/endpoints/report.py
+++ backend/app/api/api_v1/endpoints/report.py
@@ -2,37 +2,44 @@ from datetime import datetime
 from typing import List
 from fastapi import APIRouter, Depends, status
 from app import crud, exceptions, schemas
+from app.api.api_v1.endpoints.workspace import get_workspace
 from app.schemas.dbref import RefWorkspace
-from pymongo.client_session import ClientSession
+
 from app.api import deps
-from app.schemas.pyobjectid import PyObjectId
-from app.schemas.workspace import WorkspaceCreate
 from sqlalchemy.orm import Session
 from app.crud.crud_workspace import CRUDWorkspace
 from app import models
 router = APIRouter()
 
 # 3
-@router.get("/", status_code=status.HTTP_200_OK, response_model=schemas.WorkspaceOut)
-def get_report(db: Session = Depends(deps.get_db)) -> dict:
-    """
-    Root Get
-    """
-    workspace = crud.workspace.get(db=db, id=5)
-    workspace.workspace_uuid = workspace.workspace_uuid.strip().decode('ascii')
-    return workspace
-    return {"msg": "Hello, World!"}
+@router.get("/{report_id}", status_code=status.HTTP_200_OK, response_model=schemas.WorkspaceOut)
+def get_report(
+    report_id: int, 
+    db: Session = Depends(deps.get_db)
+) -> models.Report:
+    
+    return crud.report.get(db=db, id=report_id)
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.WorkspaceOut)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ReportOut)
 def create_report(
-    workspace_in: WorkspaceCreate,
+    report_in: schemas.ReportCreate,
+    reportdata_in: schemas.ReportdataCreate,
     db: Session = Depends(deps.get_db),
-    user_in: models.User = Depends(deps.get_current_user)
-) -> models.User:
+    user: models.User = Depends(deps.get_current_user),
+    workspace: models.Workspace = Depends(deps.get_current_workspace),
+) -> models.Report:
     """
     Root Get
     """
-    workspace = crud.workspace.create(db=db, user_in=user_in, obj_in=workspace_in)
+    reportdata = crud.reportdata.create(db=db, obj_in=reportdata_in)
+    
+    report = crud.report.create(
+        db=db, 
+        workspace_in=workspace, 
+        user_in=user,
+        reportdata_in=reportdata,
+        obj_in=report_in
+    )
 
-    return workspace
+    return report
