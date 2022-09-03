e229f343421322b69b62720232a1785068492117
Daniel Choi <schoiak@connect.ust.hk>
2022-09-03 23:41:18 +0900

    Mod - block was renamed to file

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import user, block, workspace, auth, member, report
+from app.api.api_v1.endpoints import file, user, workspace, auth, member, report
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user", tags=["user"])
-api_router.include_router(block.router, prefix="/block", tags=["block"])
+api_router.include_router(file.router, prefix="/file", tags=["file"])
 api_router.include_router(workspace.router, prefix="/workspace", tags=["workspace"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(member.router, prefix="/member", tags=["member"])
