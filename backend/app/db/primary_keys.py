bd366600f233872610d1d920b383cddd1c2925d8
Daniel Choi <schoiak@connect.ust.hk>
2022-09-03 23:40:33 +0900

    Mod - uuid key propeties added

--- backend/app/db/primary_keys.py
+++ backend/app/db/primary_keys.py
@@ -4,27 +4,51 @@ from typing import Literal
 
 class PKey:
     @property
-    def blocks(self):
-        return 'blocks.block_id'
+    def files_id(self):
+        return 'files.file_id'
     @property
-    def members(self):
+    def members_id(self):
         return 'members.member_id'
     @property
-    def paid_targets(self):
+    def paid_targets_id(self):
         return 'paid_targets.paid_target_id'
     @property
-    def reports(self):
+    def reports_id(self):
         return 'reports.report_id'
     @property
-    def targets(self):
+    def targets_id(self):
         return 'targets.target_id'
     @property
-    def users(self):
+    def users_id(self):
         return 'users.user_id'
     @property
-    def workspaces(self):
+    def workspaces_id(self):
         return 'workspaces.workspace_uuid'
     @property
-    def reportdata(self):
+    def reportdata_id(self):
         return 'reportdata.reportdata_id'
+    @property
+    def files_uuid(self):
+        return 'files.file_uuid'
+    @property
+    def members_uuid(self):
+        return 'members.member_uuid'
+    @property
+    def paid_targets_uuid(self):
+        return 'paid_targets.paid_target_uuid'
+    @property
+    def reports_uuid(self):
+        return 'reports.report_uuid'
+    @property
+    def targets_uuid(self):
+        return 'targets.target_uuid'
+    @property
+    def users_uuid(self):
+        return 'users.user_uuid'
+    @property
+    def workspaces_uuid(self):
+        return 'workspaces.workspace_uuid'
+    @property
+    def reportdata_uuid(self):
+        return 'reportdata.reportdata_uuid'
 pkey = PKey()
