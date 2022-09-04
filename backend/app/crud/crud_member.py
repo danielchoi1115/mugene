db517d1b76fd884b1b42e8a95a525bda583cde11
Daniel Choi <schoiak@connect.ust.hk>
2022-09-04 21:38:21 +0900

    Mod - member creation now depends on current workspace

--- backend/app/crud/crud_member.py
+++ backend/app/crud/crud_member.py
@@ -11,12 +11,15 @@ class CRUDMember(CRUDBase[models.Member, MemberCreate, MemberUpdate]):
     def get(self, db: Session, id: Any) -> Optional[models.Member]:
         return db.query(self.model).filter(self.model.member_id == id).first()
     
-    def create(self, db: Session, *, obj_in: MemberCreate) -> models.Member:
+    def create(self, db: Session, *, 
+               obj_in: MemberCreate, 
+               workspace_in: models.Workspace) -> models.Member:
         if isinstance(obj_in, dict):
             create_data = obj_in
         else:
             create_data = obj_in.dict(exclude_unset=True)
         db_obj = models.Member(**create_data)
+        db_obj.workspace_uuid = workspace_in.workspace_uuid
         db_obj.member_uuid = B64UUID().bytes
         db_obj.date_created = datetime.utcnow()
         db.add(db_obj)
