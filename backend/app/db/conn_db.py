d61fa196208fbf47408262503b2b839637b90f37
Daniel Choi <schoiak@connect.ust.hk>
2022-09-25 00:47:05 +0900

    Mod - commented unused modules out

--- backend/app/db/conn_db.py
+++ backend/app/db/conn_db.py
@@ -1,18 +1,18 @@
-from app.db.init_client import client
-from pymongo.database import Database
-from pymongo.collection import Collection
+# from app.db.init_client import client
+# from pymongo.database import Database
+# from pymongo.collection import Collection
 
 
-class initDB:
-    def __init__(self) -> None:
-        self.client = client
+# class initDB:
+#     def __init__(self) -> None:
+#         self.client = client
 
-    def getDB(self, dbName: str) -> Database | None:
-        db_name_list = self.client.list_database_names()
-        if dbName in db_name_list:
-            return client[dbName]
-        else:
-            return None
+#     def getDB(self, dbName: str) -> Database | None:
+#         db_name_list = self.client.list_database_names()
+#         if dbName in db_name_list:
+#             return client[dbName]
+#         else:
+#             return None
 
     # def getCollection(self, collectionName: str) -> Collection | None:
     #     db_name_list = self.client.list_database_names()
